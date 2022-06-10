d9553eb74a185d59e2b91cc431a91a72b3ed2e3e
Robert Fordi <krsd48@ncc1.clients.dur.ac.uk>
2022-06-10 14:13:24 +0100

    Tidied up the Hub

--- NCC Code/ml.sh
+++ NCC Code/ml.sh
@@ -14,4 +14,4 @@ source /etc/profile
 module load cuda/8.0
 
 # Run your program (replace this with your program)
-srun jupyter nbconvert --to notebook --execute ml_neptuneAI.ipynb
+srun jupyter nbconvert --to notebook --execute ml_neptuneAI_Adamax.ipynb
